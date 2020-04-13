73153dc35cb4e1bd377ae1f45773a087a000fcac
Mac <cheneak@oregonstate.edu>
2020-04-13 12:48:47 -0700

    added templates folder

--- price_agg.py
+++ price_agg.py
@@ -1,9 +1,9 @@
-from flask import Flask 
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Price Aggrigator</h1>"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
