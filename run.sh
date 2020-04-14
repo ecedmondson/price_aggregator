dd70d052a7dd84355a650d8dafb1137b99aa5f9f
Jessica Albert <alberjes@flip2.engr.oregonstate.edu>
2020-04-14 13:59:12 -0700

    Create run.sh file that installs packages from requirements.txt and launches app

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PYTHONPATH=$PWD
+
+echo "Input venv name (default is venv):"
+read venvname
+echo "Starting virtual env."
+
+#readonly sourcefile="./$venvname/bin/activate"
+#source ${sourcefile}
+
+source "$venvname/bin/activate"
+pip3 install -r requirements.txt
+
+source venv/bin/activate
+
+python app.py
