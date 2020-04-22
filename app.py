b61541473dd53ab08a15d9d9814b941f177c11af
Tanner <tannerbahora@live.com>
2020-04-22 00:33:37 -0500

    Initial connection and requirements.txt change
    Sets up connection and creates the db instance

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:admin:luckythirteen@softwareengineeringdb.clphunbfrcvm.us-east-1.rds.amazonaws.com:3306/sitedb'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
