4d7c89266389c7b0ed6b4c6840290189554b60ab
Tanner <tannerbahora@live.com>
2020-04-22 22:42:25 -0500

    Switch to OSU server

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:admin:luckythirteen@softwareengineeringdb.clphunbfrcvm.us-east-1.rds.amazonaws.com:3306/sitedb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:bahorat-db:RHvPQdC1JiKCStjg@oniddb.cws.oregonstate.edu:3306/bahorat-db'
 db = SQLAlchemy(app)
 
 @app.route("/")
