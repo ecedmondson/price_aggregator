2d4627628d490422d6ee650d1422381317c45c9d
Tanner <tannerbahora@live.com>
2020-04-23 02:02:36 -0500

    Sets up a DB connection with the ability to make queries
    Switched from SQLAlchemy package to Flaskext-mysql. Also switched databases.

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:bahorat-db:RHvPQdC1JiKCStjg@oniddb.cws.oregonstate.edu:3306/bahorat-db'
-db = SQLAlchemy(app)
+
+app.config['MYSQL_DATABASE_USER'] = 'cs361_bahorat'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1011'
+app.config['MYSQL_DATABASE_DB'] = 'cs361_bahorat'
+app.config['MYSQL_DATABASE_HOST'] = 'classmysql.engr.oregonstate.edu'
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
+#Will return a tuple from the DB given a query string
+def dbIndex(query):
+    mysql = MySQL()
+    mysql.init_app(app)
+
+    conn = mysql.connect()
+    cursor =conn.cursor()
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+    return data
+    
+#Will insert into the DB given a query string
+def dbAdd(query):
+    mysql = MySQL()
+    mysql.init_app(app)
+
+    conn = mysql.connect()
+    cursor =conn.cursor()
+
+    cursor.execute(query)
+    conn.commit()
+    return "Done"
 
 if __name__ == '__main__':
     app.run(debug=True)
