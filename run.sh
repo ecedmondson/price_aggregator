e9cf7708830c3b3991872b5991e921960558239e
Emily Edmondson <ecedmondson@gmail.com>
2020-04-28 15:10:02 -0700

    ENHANCEMENT: run.sh handles the chromedriver install

--- run.sh
+++ run.sh
@@ -11,6 +11,21 @@ echo "Starting virtual env."
 source "$venvname/bin/activate"
 pip3 install -r requirements.txt
 
+CHROME=$(python3 bin/chrome_compatibility.py)
+if [[ $CHROME -eq 0 ]]
+then
+    echo "Google Chrome stable and chromedriver seem compatible. No install needed."
+fi
+
+if [[ -n $CHROME ]]
+then
+    echo "Chromedriver seemed incompatible. Attempting install."
+    pip3 install chromedriver_installer \
+        --install-option="--chromedriver-version=${CHROME}" \
+
+fi
+
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
 python3 -m flask run -h 0.0.0.0 -p 9578 --reload
+
