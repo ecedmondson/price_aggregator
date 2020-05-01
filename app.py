ee1427bc54855497558337688c882f12a1b1f4ec
Tanner <tannerbahora@live.com>
2020-05-01 17:54:51 -0500

    Switched to creating a Database class, fixed tables and added more functions
    Also cleaned things up and moved db inside the database folder

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 from flask import Flask, render_template
-from db import *
-
+from database.db import Database
 app = Flask(__name__)
-
-dbConfig(app, "cs361_bahorat", "1011")
-initializeDb(app)
-insertCustomer(app, "Tanner", "Bahora", "tan@fna", "pass")
-
-data = findCustomer(app, "sdf@fds", "pass")
-print(data)
 
 @app.route("/")
 def home():
     return render_template('home.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
