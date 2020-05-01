655c68aa0b84e8536d9e4e825735fda21db283ef
Emily Edmondson <ecedmondson@gmail.com>
2020-05-01 12:27:14 -0700

    fun. test. please do no merge this

--- run.sh
+++ run.sh
@@ -11,7 +11,8 @@ echo "Starting virtual env."
 source "$venvname/bin/activate"
 pip3 install -r requirements.txt
 
-CHROME=$(python3 bin/chrome_compatibility.py)
+CHROME=$(python3 bin/chrome_compatibility.py --check 2>&1 >/dev/null)
+echo $CHROME
 if [[ $CHROME -eq 0 ]]
 then
     echo "Google Chrome stable and chromedriver seem compatible. No install needed."
@@ -19,10 +20,9 @@ fi
 
 if [[ -n $CHROME ]]
 then
-    echo "Chromedriver seemed incompatible. Attempting install."
-    pip3 install chromedriver_installer \
-        --install-option="--chromedriver-version=${CHROME}" \
-
+    echo "Chromedriver seemed incompatible. Attempting installi of $CHROME."
+    python3 bin/chrome_compatibility.py --install
+    python3 chromedriver_installer/setup.py install --chromedriver-version=$CHROME
 fi
 
 export FLASK_APP=app.py
