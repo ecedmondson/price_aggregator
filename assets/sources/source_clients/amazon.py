c6337896da2118c62981dede2d91f3b443c4a848
Emily Edmondson <ecedmondson@gmail.com>
2020-05-04 16:40:35 -0500

    UPDATE: hp chromebook went out of stock new on amazon :sad:

--- assets/sources/source_clients/amazon.py
+++ assets/sources/source_clients/amazon.py
@@ -24,3 +24,22 @@ class Amazon(BaseClient):
     def get_photo(self):
         # parent is id="imgTagWrapperId
         return self.soup.find(id="landingImage").attrs["src"]
+
+class AmazonUsed(Amazon):
+    """ A (hopefully) re-usable Amazon web-scraping client for used content."""
+    source = "Amazon"
+    use_status = "Used"
+
+    def __init__(self, product_name, product_url):
+        self.product_name = product_name
+        self.product_url = product_url
+        super().__init__()
+        self.scraper.add(
+            document=lambda: self.get(self.product_url),
+            soup=lambda: BeautifulSoup(self.document.text, features="html5lib"),
+        )
+
+    def get_price(self):
+        # usedBuySection only works with New products
+        return self.soup.find(id="price_inside_buybox").text.strip().replace("Buy used:", "")
+
