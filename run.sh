bccbbc16c3b5608ffa5d8848c4d151168abd8f2f
Emily Edmondson <ecedmondson@gmail.com>
2020-05-04 12:16:38 -0500

    UPDATE: change run chromedriver logic

--- run.sh
+++ run.sh
@@ -5,13 +5,11 @@ echo "Input venv name (default is venv):"
 read venvname
 echo "Starting virtual env."
 
-#readonly sourcefile="./$venvname/bin/activate"
-#source ${sourcefile}
-
 source "$venvname/bin/activate"
 pip3 install -r requirements.txt
 
-CHROME=$(python3 bin/chrome_compatibility.py)
+CHROME=$(python3 bin/chrome_compatibility.py --check 2>&1 >/dev/null)
+echo $CHROME
 if [[ $CHROME -eq 0 ]]
 then
     echo "Google Chrome stable and chromedriver seem compatible. No install needed."
@@ -19,13 +17,11 @@ fi
 
 if [[ -n $CHROME ]]
 then
-    echo "Chromedriver seemed incompatible. Attempting install."
-    pip3 install chromedriver_installer \
-        --install-option="--chromedriver-version=${CHROME}" \
-
+    echo "Chromedriver seemed incompatible. Attempting installi of $CHROME."
+    python3 bin/chrome_compatibility.py --install
+    python3 chromedriver_installer/setup.py install --chromedriver-version=$CHROME
 fi
 
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
 python3 -m flask run -h 0.0.0.0 -p 9578 --reload
-
