03cd3c487bb28f3a673377051544b22141677f23
cheneak <cheneak@oregonstate.edu>
2020-05-07 11:42:18 -0700

    added listings to be displayed dynamically. Item list in app.py is temporary until data is integrated

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+items = [
+    {
+        'name': 'best buy_14DA0012DX_hp_chromebook',
+        'source': 'Best Buy',
+        'price': '$599.00',
+        'photo': 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6365/6365772_sd.jpg;maxHeight=640;maxWidth=550',
+        'instock': 'Likely In Stock, Check Retailer',
+        'new': 'New',
+        'price_check': '2020-04-30 18:38:19.752171',
+    },
+    {
+        'name': 'best buy_14DA0012DX_hp_chromebook',
+        'source': 'Best Buy',
+        'price': '$599.00',
+        'photo': 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6365/6365772_sd.jpg;maxHeight=640;maxWidth=550',
+        'instock': 'Likely In Stock, Check Retailer',
+        'new': 'New',
+        'price_check': '2020-04-30 18:38:19.752171',
+    },
+]
+
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/listings")
+def listings():
+    return render_template('listings.html', items=items)
 
 if __name__ == '__main__':
     app.run(debug=True)
