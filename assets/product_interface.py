afc7d30d133fadd9685b41e8fb6541306fda285d
Emily Edmondson <ecedmondson@gmail.com>
2020-05-16 10:11:50 -0700

    FEATURE: product interface to be used with app

---
+++ assets/product_interface.py
@@ -0,0 +1,66 @@
+from assets.products.product_clients.chromebook import HPTouchScreenChromebook
+from assets.products.product_clients.macbookair import MacBookAir2020
+from assets.sources.base_client import cache
+from itertools import chain
+from time import time
+
+def list_from(item):
+    if not item:
+        return []
+    if isinstance(item, (str, dict)) or not is_iterable(
+        item
+    ):
+        return [item]
+    return list(item)
+
+class ProductInterface:
+    """Interface object so that the UI can easily obtain filtered products."""
+    def __init__(self):
+        self.all_products = [HPTouchScreenChromebook(), MacBookAir2020()]
+
+    def scrape(self, cache_id="interface_scrape"):
+        print("DEBUG: retrieval of products...")
+        start = time()
+        p = list(chain.from_iterable([x.products for x in self.all_products]))
+        print(f"Retrieval took {time() - start} seconds.")
+        return p
+
+    def get_products_by_filters(self, json=True, product_type=None, sources_to_exclude=None, price_ceiling=None, use_status=None):
+        """Pass filters as kwargs
+        
+        Filter options:
+            - product_type (str): 'computer' or 'tablet'
+            - sources_to_exclude (str): a retailer, i.e. 'amazon'
+            - price_ceiling (int): highest price
+            - use_status (str): use status, i.e. new or used
+        """
+
+        all_products = self.scrape()
+        print(len(all_products))
+        product_type = list_from(product_type) or []
+        sources_to_exclude = list_from(sources_to_exclude) or []
+        price_ceiling = price_ceiling or 0
+        use_status = list_from(use_status) or []
+        # List typecast since chain is consumed upon iteration
+        exclude = list(chain(product_type, sources_to_exclude, use_status))
+
+        def desirable(product):
+            """Takes a ScrapedProduct object and returns user-desirability boolean."""
+            prod_chars = [getattr(product, x) for x in ["product_type", "source", "price_n", "new"]]
+            print(prod_chars)
+            print(sources_to_exclude)
+            print(exclude)
+            exclusion_matches = [
+                x in exclude if not isinstance(x, int) else x < price_ceiling
+                for x in prod_chars
+            ]
+            print(exclusion_matches)
+            print(any(exclusion_matches))
+            return any(exclusion_matches)
+        
+        filtered = list(filter(lambda p: not desirable(p), all_products)) 
+        print([x.jsonify() for x in filtered])
+        if json:
+            return [x.jsonify() for x in filtered]
+        return filtered
+
