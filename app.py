8b35a7d03e744040c1b89e5f89b2821ebe5891b5
Emily Edmondson <ecedmondson@gmail.com>
2020-05-16 11:10:32 -0700

    HW-07 Requirement: Refactoring out dead code

--- app.py
+++ app.py
@@ -16,30 +16,9 @@ class User(UserMixin):
   def __init__(self,id):
     self.id = id
 
-items = [
-    {
-        'name': 'best buy_14DA0012DX_hp_chromebook',
-        'source': 'Best Buy',
-        'price': '$599.00',
-        'photo': 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6365/6365772_sd.jpg;maxHeight=640;maxWidth=550',
-        'instock': 'Likely In Stock, Check Retailer',
-        'new': 'New',
-        'price_check': '2020-04-30 18:38:19.752171',
-    },
-    {
-        'name': 'best buy_14DA0012DX_hp_chromebook',
-        'source': 'Best Buy',
-        'price': '$599.00',
-        'photo': 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6365/6365772_sd.jpg;maxHeight=640;maxWidth=550',
-        'instock': 'Likely In Stock, Check Retailer',
-        'new': 'New',
-        'price_check': '2020-04-30 18:38:19.752171',
-    },
-]
 
 @app.route("/")
 def home():
-    print(db.findCustomer("hello123@gmail.com","12345678"))
     return render_template('home.html')
 
 @app.route("/login", methods=["GET", "POST"])
