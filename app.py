0470a49efcaafdf668980c79f9cdc9d08fc510ec
Emily Edmondson <ecedmondson@gmail.com>
2020-05-24 12:03:59 -0700

    NEW: can filter products. also did a lift and shift of ProductDBInterface

--- app.py
+++ app.py
@@ -38,6 +38,79 @@ items = [
     },
 ]
 
+#####################
+# PRODUCT INTERFACE #
+#####################
+
+def list_from(item):
+    if not item:
+        return []
+    if isinstance(item, (str, dict)) or not is_iterable(
+        item
+    ):
+        return [item]
+    return list(item)
+
+
+class ProductDBInterface:
+    """Interface object so that the UI can easily obtain filtered products."""
+    def parse_sql_tuple(self, x):
+        return (ScrapedProduct(name = x[2], source = x[5], price = x[3], photo = x[4], instock= x[6], new = x[7], price_check= x[8]))
+
+    @property
+    def read_products_from_db(self):
+        prod_tuple = db.getRetailers_Products()
+        products = [self.parse_sql_tuple(x) for x in prod_tuple]
+        return products
+
+
+    def filter_products(self, json=False, product_type=None, sources_to_exclude=None, price_ceiling=None, use_status=None):
+        """Pass filters as kwargs
+        
+        Filter options:
+            - product_type (str): 'computer' or 'tablet'
+            - sources_to_exclude (str): a retailer, i.e. 'amazon'
+            - price_ceiling (int): highest price
+            - use_status (str): use status, i.e. new or used
+        """
+        all_products = self.read_products_from_db
+        product_type = list_from(product_type) or []
+        sources_to_exclude = list_from(sources_to_exclude) or []
+        price_ceiling = price_ceiling or 0
+        use_status = list_from(use_status) or []
+        # List typecast since chain is consumed upon iteration
+        exclude = list(chain(product_type, sources_to_exclude, use_status))
+
+        def desirable(product):
+            """Takes a ScrapedProduct object and returns user-desirability boolean."""
+            prod_chars = [getattr(product, x) for x in ["source", "price_n", "new"]]
+            exclusion_matches = [
+                x in exclude if not isinstance(x, int) else x < price_ceiling
+                for x in prod_chars
+            ]
+            return any(exclusion_matches)
+
+        filtered = list(filter(lambda p: not desirable(p), all_products))
+        print(filtered)
+        if json:
+            return [x.jsonify() for x in filtered]
+        return filtered
+
+interface = ProductDBInterface()
+
+def unique_json_key(p):
+    """Returns a unique JSON Key based on ScrapedProduct data. Takes a JSON blob/dict."""
+    return f"{x['source'].lower()}_{x['name'].lower().replace(' ', '_')}"
+
+@app.route("/resources/products", methods=["POST"])
+def search_products_api(**kwargs):
+    """This endpoint makes it easier to test filtering.
+       Leaving it in for now because we still need to implement
+       product_type and that will need to be tested.
+    """
+    if request.method == 'POST':
+        return { unique_json_key(x): x for x in interface.filter_products(json=True, **request.json)}
+
 @app.route("/")
 def home():
     print(db.findCustomer("hello123@gmail.com","12345678"))
