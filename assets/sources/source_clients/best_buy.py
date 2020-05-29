2806b5c37fc5a917c9e2d0234eef395b8686bf3b
Emily Edmondson <ecedmondson@gmail.com>
2020-05-28 19:45:53 -0700

    ENHANCEMENT: get price is better and add backup file

--- assets/sources/source_clients/best_buy.py
+++ assets/sources/source_clients/best_buy.py
@@ -10,7 +10,8 @@ class BestBuy(BaseClient):
         self.product_name = product_name
         self.product_url = product_url
         self.filename = f"{self.source.lower()}_{self.product_name}"
-        super().__init__(**kwargs)
+        self.backup_file=f"{self.product_name}/{self.source.lower().replace(' ', '_')}.html"
+        super().__init__()
         self.scraper.add(
             document=lambda: self.dynamic_get(self.product_url),
             soup=lambda: bs4.BeautifulSoup(self.document, features="html.parser"),
@@ -23,16 +24,20 @@ class BestBuy(BaseClient):
         return "Sold Out" in add_to_cart_button.text
 
     def get_price(self):
-        price_boxes = self.soup.find_all(attrs={"class": "priceView-layout-large"})
-        correct_box = [
-            x
-            for box in price_boxes
-            for x in box.contents
-            if "Price Match Guarantee" in x.text
-        ]
-        text_content = correct_box[0].text
-        text_content = text_content.replace("Price Match Guarantee", "")
-        return text_content[: text_content.find("Your price")]
+        try:
+            price = self.soup.find(attrs={"class": "price-box"}).find(attrs={"class": "priceView-customer-price"})
+            return price.text.split(" ")[-1]
+        except Exception:
+            price_boxes = self.soup.find_all(attrs={"class": "priceView-layout-large"})
+            correct_box = [
+                x
+                for box in price_boxes
+                for x in box.contents
+                if "Price Match Guarantee" in x.text
+            ]
+            text_content = correct_box[0].text
+            text_content = text_content.replace("Price Match Guarantee", "")
+            return text_content[: text_content.find("Your price")]
 
     def get_photo(self):
         return self.soup.find(attrs={"class": "primary-image"}).attrs["src"]
